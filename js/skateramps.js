df223752625b475161394bf191c8ecfe1fdf2103
Icedoomer11 <156988725+Icedoomer11@users.noreply.github.com>
2024-05-23 15:30:13 +0500

    Clean up
    Made the page cleaner
    Now you can orbit
    Previous and next
    Grey colored

--- js/skateramps.js
+++ js/skateramps.js
@@ -5,27 +5,36 @@ let textureColor;
 function preload() {
   ramps.push(loadModel('obj/ramp1.obj'));
   ramps.push(loadModel('obj/ramp2.obj'));
+  ramps.push(loadModel('obj/ramp3.obj'));
+  ramps.push(loadModel('obj/ramp4.obj'));
+  // Add more ramps if needed
 }
 
 function setup() {
   const Sketch1 = document.querySelector('.sketch1'); // Get the container element
 
-  const canvas = createCanvas(500, 500, WEBGL); // Create WebGL canvas
+  const canvas = createCanvas(1000, 500, WEBGL); // Create WebGL canvas
   canvas.parent(Sketch1); // Set the parent of the canvas to the container element
 
   // Define the texture color
-  textureColor = color(188, 106, 60); // Red color (change as needed)
+  textureColor = color('#808080'); // Red color (change as needed)
 
   camera(200, -100, 100);
 
-  // Create a button
-  const button = createButton('Cycle Ramp');
-  button.mousePressed(cycleRamp);
+  // Create buttons
+  const prevButton = createButton('Prev Ramp');
+  prevButton.mousePressed(prevRamp);
+  
+  const nextButton = createButton('Next Ramp');
+  nextButton.mousePressed(nextRamp);
 }
 
 function draw() {
   background(245,245,245);
   lights();
+
+  let c = color(100, 100, 100);
+  directionalLight(c, 0, 1, 0);
 
   // Draw the current ramp
   const currentRamp = ramps[currentRampIndex];
@@ -33,18 +42,22 @@ function draw() {
     fill(textureColor); // Set the fill color
     noStroke(); // Disable stroke 
 
-    rotateX(PI);
-    rotateY(frameCount * 0.005);
+    orbitControl();
     scale(20); // Adjust the scale to fit the ramp properly
     model(currentRamp);
   }
 }
 
-function cycleRamp() {
-  // Increment the current ramp index
+function nextRamp() {
   currentRampIndex++;
-  // Wrap around to the beginning if we reach the end
   if (currentRampIndex >= ramps.length) {
     currentRampIndex = 0;
+  }
+}
+
+function prevRamp() {
+  currentRampIndex--;
+  if (currentRampIndex < 0) {
+    currentRampIndex = ramps.length - 1;
   }
 }
